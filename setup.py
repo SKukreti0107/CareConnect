2129ee5dcffb9ac0a3a0bd805a4eefdf59f1339d
SKukreti0107 <shubhamkukreti.0107@gmail.com>
2025-03-12 13:08:54 +0530

    initial project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name= "careConnect",
+    version="1.0.0",
+    author="Shubham",
+    author_email="shubhamkukreti.0107@gmail.com",
+    packages = find_packages(),
+    install_require = []
+)
